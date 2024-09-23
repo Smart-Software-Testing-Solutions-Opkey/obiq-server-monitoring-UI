e2a23fa2ab0b2b679f02cd5328de7da469011e8c
Arpan Stephen <144207307+ArpanOpkey@users.noreply.github.com>
2024-09-23 16:17:42 +0530

    modal-curd-add environment

--- server-monitoring/src/app/environment/curd/add/environment-curd.component.ts
+++ server-monitoring/src/app/environment/curd/add/environment-curd.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-environment-curd',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./environment-curd.component.scss']
 })
 export class EnvironmentCurdComponent {
+
+  constructor(
+    public activeModal: NgbActiveModal
+  ) { }
+
+
+  create_environment() {
+    debugger;
+  }
+
+  close_model() {
+    this.activeModal.dismiss('Cross click');
+  }
 
 }
--- server-monitoring/src/app/environment/manager/environment-manager.component.ts
+++ server-monitoring/src/app/environment/manager/environment-manager.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EnvironmentCurdComponent } from '../curd/add/environment-curd.component';
 
 @Component({
   selector: 'app-environment-manager',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./environment-manager.component.scss']
 })
 export class EnvironmentManagerComponent {
+
+  constructor(
+
+    private modalService: NgbModal,
+  
+  ) { }
+
+  ngOnInit(): void {
+    this.curd_create_environment();
+  }
+
+  curd_create_environment() {
+    const modalRef = this.modalService.open(EnvironmentCurdComponent, {
+      backdrop: 'static',
+      keyboard: false,
+      size: 'md',
+      centered: true,
+      windowClass: 'layout-modal'
+    });
+    modalRef.result.then((result) => {
+    }, (response) => {
+      if (response == 'Cross click') {
+        return;
+      }
+    });
+  }
 
 }
