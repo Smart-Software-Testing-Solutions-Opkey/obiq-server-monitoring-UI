6ccaeb08673ec52049649e9ac2d360edb2592f03
Abhishekparihar08 <pariharabhishek690@gmail.com>
2024-10-08 18:39:06 +0530

    2nd page ui

--- server-monitoring/src/app/environment/curd/add/environment-curd.component.ts
+++ server-monitoring/src/app/environment/curd/add/environment-curd.component.ts
@@ -27,6 +27,7 @@ export class EnvironmentCurdComponent {
   select_datasource = [
     {
       displayname: "ERP Analytics",
+      displayType: "ERPAnalytics",
       title: "ERP is a software system that integrates core business processes into a single platform to improve efficiency and decision-making.",
       subtitle: "To display more ERPs go to instance and select ESS logs.",
       display_services: true,
@@ -42,17 +43,20 @@ export class EnvironmentCurdComponent {
     },
     {
       displayname: "User Behavior Analytics",
+      displayType: "UserBehaviorAnalytics",
       title: "ERP is a software system that integrates core business processes into a single platform to improve efficiency and decision-making.",
       subtitle: "",
       display_services: true,
       showServices: false, 
       services: [
-        { displayname: "Oracle Fusion", icon: 'OracleFusion'},
-        { displayname: "SAP", icon: 'SAP'},
-        { displayname: "Salesforce", icon: 'Salesforce'},
-        { displayname: "Workday", icon: 'Workday'},
-        { displayname: "PeopleSoft", icon: 'PeopleSoft'},
+        { displayname: "Olivia Rhye", email: 'Olivia.rhye@opkey.com', icon: 'profile'},
+        { displayname: "Phoenix Baker", email: 'Phoenix.baker@opkey.com', icon: 'profile'},
+        { displayname: "Lana Steiner", email: 'Lana.steiner@opkey.com', icon: 'profile'},
+        { displayname: "Demi Wilkinson", email: 'Demi.wilkinson@opkey.com', icon: 'profile'},
+        { displayname: "Candice Wu", email: 'Candice.wu@opkey.com', icon: 'profile'},
+        { displayname: "Candice Wu", email: 'Candice.wu@opkey.com', icon: 'profile'},
         { displayname: "Oracle EBS", icon: 'OracleEBS'},
+
       ]
     },
 ];
