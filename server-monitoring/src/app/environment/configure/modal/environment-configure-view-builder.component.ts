6f15d6b98bc00e97d3a372e59940c0e0b12c5bf3
upendrasharma248 <69623902+upendrasharma248@users.noreply.github.com>
2024-10-08 08:59:07 +0530

    modal view builder

--- server-monitoring/src/app/environment/configure/modal/environment-configure-view-builder.component.ts
+++ server-monitoring/src/app/environment/configure/modal/environment-configure-view-builder.component.ts
@@ -17,7 +17,38 @@ export class EnvironmentConfigureViewBuilderComponent {
   }
 
   next() {
-    
+
   }
+
+  select_datasource = [
+    {
+      displayname: "ERP Analytics",
+      title: "ERP is a software system that integrates core business processes into a single platform to improve efficiency and decision-making.",
+      subtitle: "To display more ERPs go to instance and select ESS logs.",
+      display_subitem: true,
+      subitem: [
+        { displayname: "Oracle Fusion", icon: 'OracleFusion'},
+        { displayname: "SAP", icon: 'SAP'},
+        { displayname: "Salesforce", icon: 'Salesforce'},
+        { displayname: "Workday", icon: 'Workday'},
+        { displayname: "PeopleSoft", icon: 'PeopleSoft'},
+        { displayname: "Oracle EBS", icon: 'OracleEBS'},
+      ]
+    },
+    {
+      displayname: "User Behavior Analytics",
+      title: "ERP is a software system that integrates core business processes into a single platform to improve efficiency and decision-making.",
+      subtitle: "",
+      display_subitem: true,
+      subitem: [
+        { displayname: "Oracle Fusion", icon: 'OracleFusion'},
+        { displayname: "SAP", icon: 'SAP'},
+        { displayname: "Salesforce", icon: 'Salesforce'},
+        { displayname: "Workday", icon: 'Workday'},
+        { displayname: "PeopleSoft", icon: 'PeopleSoft'},
+        { displayname: "Oracle EBS", icon: 'OracleEBS'},
+      ]
+    },
+  ]
   
 }
