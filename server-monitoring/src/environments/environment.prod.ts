bfd8089f7645d9f8dfa41420855c22c85f690c48
kunalmax <68528884+kunalmax@users.noreply.github.com>
2024-10-09 05:40:29 +0000

    environment files added

---
+++ server-monitoring/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+
+  production: false,
+  BASE_OPKEY_URL: "/",
+  BASE_OBIQ_OPKEY_URL: window.BASE_OBIQ_OPKEY_URL,
+  BASE_INDEXING_URL: "/",
+  BASE_EVOLVE_URL:  "/",
+  BASE_ReportingV2_URL: "/",
+  Prefix_OBIQ_Server: "/"
+  
+};
---
+++ server-monitoring/src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+
+  production: false,
+  BASE_OPKEY_URL: window.base_url,
+  BASE_OBIQ_OPKEY_URL: window.BASE_OBIQ_OPKEY_URL,
+  BASE_INDEXING_URL: "/",
+  BASE_EVOLVE_URL: "https://localhost:50173/",
+  BASE_ReportingV2_URL: "/",
+  Prefix_OBIQ_Server: "/"
+  
+};
