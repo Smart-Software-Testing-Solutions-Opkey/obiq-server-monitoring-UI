92566a805ac6c20fc596e96d3d40d79f5ae1e70c
Utkarsh-Hiranwal <153630089+Utkarsh-Hiranwal@users.noreply.github.com>
2024-10-30 11:56:19 +0530

    health bar component

---
+++ server-monitoring/src/app/environment/manager/widgets/environment-manager-widgets-health-bars/environment-manager-widgets-health-bars.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-environment-manager-widgets-health-bars',
+  templateUrl: './environment-manager-widgets-health-bars.component.html',
+  styleUrl: './environment-manager-widgets-health-bars.component.scss'
+})
+export class EnvironmentManagerWidgetsHealthBarsComponent {
+
+}
