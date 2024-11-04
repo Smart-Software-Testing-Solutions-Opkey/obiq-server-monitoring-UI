dd86a1a4ed082b68288ed456c864b12b1d5c8d4e
Himanshuopkey <139320785+Himanshuopkey@users.noreply.github.com>
2024-11-04 15:35:36 +0530

    Revert "added to update"
    This reverts commit 370baa8e628d8987201673d4c8c5e8f85d0fb6d7.

--- server-monitoring/src/environments/environment.ts
+++ server-monitoring/src/environments/environment.ts
@@ -5,7 +5,6 @@ export const environment = {
   BASE_OPKEY_URL: window.base_url,
   BASE_OBIQ_SERVER_URL: window.base_obiq_server_url,
   Dev_Enviornment: "https://myqlm.dev.opkeyone.com",
-  pre_prodEnviornment:"https://myqlm.preprod.opkeyone.com",
   BASE_OBIQ_OPKEY_URL: "/",
   BASE_INDEXING_URL: "/",
   BASE_EVOLVE_URL: "https://localhost:50173/",
