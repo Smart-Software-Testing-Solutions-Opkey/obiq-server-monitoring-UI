69bc02f76fae1d807e97169fee6e4eab2e18b879
Himanshuopkey <139320785+Himanshuopkey@users.noreply.github.com>
2024-11-08 15:03:24 +0530

    added enum and notification service

---
+++ server-monitoring/src/app/services/notification-service/notifications.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+//import { NotificationService } from '@progress/kendo-angular-notification';
+import { NotificationType } from 'src/app/global/enums';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationsService {
+
+  // constructor(private notificationService: NotificationService) { }
+
+  // public notifier(type: NotificationType, content, customHideAfter?: number): void {
+  //   const hideAfter = customHideAfter !== undefined ? customHideAfter : 2500;
+  //   this.notificationService.show({
+  //     content: content,
+  //     hideAfter: hideAfter,
+  //     cssClass: 'notification-box',
+  //     position: { horizontal: "right", vertical: "bottom" },
+  //     animation: { type: "fade", duration: 600 },
+  //     type: { style: type, icon: true },
+  //     closable: false,
+  //   });
+  // }
+}
