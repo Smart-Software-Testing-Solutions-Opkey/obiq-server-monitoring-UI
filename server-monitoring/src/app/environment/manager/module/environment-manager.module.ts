6aee02f8de9b1164e902e3e5d9443cb12ae3231c
Priya Kumari <priya.kumari@opkey.com>
2024-12-16 16:40:18 +0530

    Revert "Merge branch 'release-v1.0' of https://github.com/pankyopkey/obiq-server-monitoring-UI into release-v1.0"
    This reverts commit fbce53ae289f6c24c075654b6b88bd2aa9a79633, reversing
    changes made to ef69a233d8965e4407190a0109167408ba3e003d.

--- server-monitoring/src/app/environment/manager/module/environment-manager.module.ts
+++ server-monitoring/src/app/environment/manager/module/environment-manager.module.ts
@@ -38,9 +38,6 @@ import { LabelModule } from "@progress/kendo-angular-label";
 import { DateInputsModule } from "@progress/kendo-angular-dateinputs";
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { GridsterComponent, GridsterItemComponent } from 'angular-gridster2';
-import { EMMrDsErpOverviewTabComponent } from '../main-right/datasources/erp/e-m-mr-ds-erp-overview-tab/e-m-mr-ds-erp-overview-tab.component';
-import { EMMrDsErpLogTabComponent } from '../main-right/datasources/erp/e-m-mr-ds-erp-log-tab/e-m-mr-ds-erp-log-tab.component';
-import { EMMrDsErpFunctionalErrorTabComponent } from '../main-right/datasources/erp/e-m-mr-ds-erp-functional-error-tab/e-m-mr-ds-erp-functional-error-tab.component';
 
 @NgModule({
   declarations: [
@@ -67,10 +64,7 @@ import { EMMrDsErpFunctionalErrorTabComponent } from '../main-right/datasources/
     EnvironmentManagerWidgetsHealthBarsComponent,
     ManagerMainRightLogTabDetailsInfoComponent,
     EnvironmentManagerWidgetsProgressBarsComponent,
-    SelectedTraceInnerComponent,
-    EMMrDsErpOverviewTabComponent,
-    EMMrDsErpLogTabComponent,
-    EMMrDsErpFunctionalErrorTabComponent
+    SelectedTraceInnerComponent
 
 
   ],
