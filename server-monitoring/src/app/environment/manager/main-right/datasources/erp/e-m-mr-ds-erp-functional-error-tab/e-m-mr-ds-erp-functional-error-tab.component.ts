ca374e337db87205f31bd0be299a77350ab86f67
Priya Kumari <priya.kumari@opkey.com>
2024-12-16 16:47:54 +0530

    Reapply "Merge branch 'release-v1.0' of https://github.com/pankyopkey/obiq-server-monitoring-UI into release-v1.0"
    This reverts commit 6aee02f8de9b1164e902e3e5d9443cb12ae3231c.

---
+++ server-monitoring/src/app/environment/manager/main-right/datasources/erp/e-m-mr-ds-erp-functional-error-tab/e-m-mr-ds-erp-functional-error-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-m-mr-ds-erp-functional-error-tab',
+  templateUrl: './e-m-mr-ds-erp-functional-error-tab.component.html',
+  styleUrl: './e-m-mr-ds-erp-functional-error-tab.component.scss'
+})
+export class EMMrDsErpFunctionalErrorTabComponent {
+
+}
---
+++ server-monitoring/src/app/environment/manager/main-right/datasources/erp/e-m-mr-ds-erp-log-tab/e-m-mr-ds-erp-log-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-m-mr-ds-erp-log-tab',
+  templateUrl: './e-m-mr-ds-erp-log-tab.component.html',
+  styleUrl: './e-m-mr-ds-erp-log-tab.component.scss'
+})
+export class EMMrDsErpLogTabComponent {
+
+}
