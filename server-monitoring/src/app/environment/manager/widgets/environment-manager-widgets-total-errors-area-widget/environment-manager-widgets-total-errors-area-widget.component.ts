09ef7f1f27207d4de148ad7f07a77a42b935a17b
Priya Kumari <priya.kumari@opkey.com>
2024-12-16 17:44:51 +0530

    drag function update

--- server-monitoring/src/app/environment/manager/widgets/environment-manager-widgets-total-errors-area-widget/environment-manager-widgets-total-errors-area-widget.component.ts
+++ server-monitoring/src/app/environment/manager/widgets/environment-manager-widgets-total-errors-area-widget/environment-manager-widgets-total-errors-area-widget.component.ts
@@ -51,7 +51,7 @@ export class EnvironmentManagerWidgetsTotalErrorsAreaWidgetComponent implements 
   ) {
     
   }
-
+@Input()Editable:boolean
   ngOnInit(){
     this.subscriptions.push(this.app_service.dataStream$.subscribe((data: any) => {
       if(data?.type == "getDataWithTime"){
