7e1e11b01d670232c6edfc191d6f52c2776f1d38
Himanshuopkey <139320785+Himanshuopkey@users.noreply.github.com>
2024-12-18 15:32:22 +0530

    dded Functional Error logs

--- server-monitoring/src/app/environment/manager/main-right/datasources/erp/e-m-mr-ds-erp-functional-error-tab/e-m-mr-ds-erp-functional-error-tab.component.ts
+++ server-monitoring/src/app/environment/manager/main-right/datasources/erp/e-m-mr-ds-erp-functional-error-tab/e-m-mr-ds-erp-functional-error-tab.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AppDataService } from 'src/app/services/app-data.service';
+import { AppService } from 'src/app/services/app.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-e-m-mr-ds-erp-functional-error-tab',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrl: './e-m-mr-ds-erp-functional-error-tab.component.scss'
 })
 export class EMMrDsErpFunctionalErrorTabComponent {
+  constructor(
+      public service_data: AppDataService,
+      public app_service: AppService,
+      public dataService: AppDataService,
+  ){
 
+  }
+@Input() analyticsType: any;
+  @Input() view: any;
+
+
+  ngOnInit(): void {
+  this.get_Functional_log_error();
+  }
+    get_Functional_log_error() {
+      let form_url = environment.BASE_OBIQ_SERVER_URL + "OpkeyObiqServerApi/OpkeyTraceIAAnalyticsApi/ErrorDataAnalyticController/getAllAppFunctionalErrorByFilter";
+  
+      let form_data = {
+        "timeSpanEnum": "LAST_7_DAYS",
+        "limitBy": 0,
+        "appType":"ORACLEFUSION",
+        "offset": 0,
+      };
+  
+      this.app_service.make_post_server_call(form_url, form_data).subscribe({
+        next: (result: any) => {
+       
+        },
+        error: (error: any) => {
+          // window.loadingStop("#Env_manager_main_right");
+          console.warn(error);
+        },
+        complete: () => {
+          console.log("Completed");
+        }
+      });
+    }
 }
