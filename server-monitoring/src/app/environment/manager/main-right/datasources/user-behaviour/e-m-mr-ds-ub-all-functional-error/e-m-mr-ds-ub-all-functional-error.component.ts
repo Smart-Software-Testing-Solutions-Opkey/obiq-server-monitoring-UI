3ef427d598b251c54ec637b55ab20db3518363aa
PoojaJanaOpkey <pooja.jana@opkey.com>
2025-01-21 00:03:41 +0530

    all functional , all api and all console pages app log panel added

--- server-monitoring/src/app/environment/manager/main-right/datasources/user-behaviour/e-m-mr-ds-ub-all-functional-error/e-m-mr-ds-ub-all-functional-error.component.ts
+++ server-monitoring/src/app/environment/manager/main-right/datasources/user-behaviour/e-m-mr-ds-ub-all-functional-error/e-m-mr-ds-ub-all-functional-error.component.ts
@@ -35,25 +35,25 @@ ngOnInit(): void {
 this.get_Functional_log_error();
 this.startDataReceiving();
 }
-//  onSelectionChange(e) {
-//    console.log(this.analyticsType,"this is analytics type ");
-//    console.log(this.view,"this is the view")
-//     let dataItem = e.dataItem
-//     const modalRef = this.modalService.open(ManagerRightPanelComponent, {
-//       backdrop: 'static',
-//       keyboard: false,
-//       size: 'full',
-//       centered: true,
-//       windowClass: 'layout-modal-right panel-end w-75'
-//     });
-//     modalRef.result.then((result) => {
-//     }, (response) => {
-//       if (response == 'close modal') {
-//         return;
-//       }
-//     });
-//     modalRef.componentInstance.selectedItem = { callsource: 'Erp_functional_logs_Journey_pannel', data: dataItem };
-//  }
+ onSelectionChange(e) {
+   console.log(this.analyticsType,"this is analytics type ");
+   console.log(this.view,"this is the view")
+    let dataItem = e.dataItem
+    const modalRef = this.modalService.open(ManagerRightPanelComponent, {
+      backdrop: 'static',
+      keyboard: false,
+      size: 'full',
+      centered: true,
+      windowClass: 'layout-modal-right panel-end w-75'
+    });
+    modalRef.result.then((result) => {
+    }, (response) => {
+      if (response == 'close modal') {
+        return;
+      }
+    });
+    modalRef.componentInstance.selectedItem = { callsource: 'ub_functional_logs_panel', data: dataItem };
+ }
 
 logToSearch : any;
 appType : string = 'ORACLEFUSION'
