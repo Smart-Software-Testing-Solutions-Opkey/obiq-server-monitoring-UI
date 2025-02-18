d495506e7e4b0420290ffa6054dc8fe4da875d4f
PoojaJanaOpkey <pooja.jana@opkey.com>
2025-02-18 18:52:25 +0530

    Revert "release mode command changes"
    This reverts commit 4e18af4b8aa163c0317828652130e3384592ceac.

--- server-monitoring/src/assets/keycloak/opkey_keycloak_functions.js
+++ server-monitoring/src/assets/keycloak/opkey_keycloak_functions.js
@@ -14,7 +14,7 @@ var KeycloakClientName = '';
 
 //var pre_url= window.location.href.indexOf("localhost")>-1? window.base_url:"";
 
-var pre_url = window.location.href.indexOf("localhost:4200")>-1? window.base_url+ '/':"";
+var pre_url = window.base_url;
 
 //This method must now be deprecated as its name is mis-leading
 function get_keycloak_attribute(keycloakClientName) {
@@ -29,7 +29,7 @@ function keycloak_init_with_force_login(keycloakClientName, initedCallback) {
 
     let xhr = new XMLHttpRequest();
     KeycloakClientName = keycloakClientName;
-    xhr.open('GET', pre_url + 'login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
+    xhr.open('GET', pre_url + '/login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
 
     xhr.onload = function () {
 
@@ -158,7 +158,7 @@ function keycloak_init_without_force_login(keycloakClientName, initedCallback) {
 
     let xhr = new XMLHttpRequest();
     KeycloakClientName = keycloakClientName;
-    xhr.open('GET', pre_url + 'login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
+    xhr.open('GET', pre_url + '/login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
 
     xhr.onload = function () {
 
@@ -232,7 +232,7 @@ function set_keycloak_token(token) {
 
 
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', pre_url + 'login/create_keycloak_token_cookie?token=' + token);
+    xhr.open('POST', pre_url + '/login/create_keycloak_token_cookie?token=' + token);
     xhr.send();
 
     xhr.onload = function () {
