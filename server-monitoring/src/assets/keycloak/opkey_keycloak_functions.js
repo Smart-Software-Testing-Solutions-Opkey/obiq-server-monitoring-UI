10afd66c112f2ebce0d1bb2b269ec238f80cd2ed
PoojaJanaOpkey <pooja.jana@opkey.com>
2025-04-14 11:51:34 +0530

    API updated get_keycloak_settings

--- server-monitoring/src/assets/keycloak/opkey_keycloak_functions.js
+++ server-monitoring/src/assets/keycloak/opkey_keycloak_functions.js
@@ -29,7 +29,7 @@ function keycloak_init_with_force_login(keycloakClientName, initedCallback) {
 
     let xhr = new XMLHttpRequest();
     KeycloakClientName = keycloakClientName;
-    xhr.open('GET', pre_url + 'login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
+    xhr.open('POST', pre_url + 'login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
 
     xhr.onload = function () {
 
@@ -158,7 +158,7 @@ function keycloak_init_without_force_login(keycloakClientName, initedCallback) {
 
     let xhr = new XMLHttpRequest();
     KeycloakClientName = keycloakClientName;
-    xhr.open('GET', pre_url + 'login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
+    xhr.open('POST', pre_url + 'login/get_keycloak_settings?opkeyone_callsource=' + keycloakClientName);
 
     xhr.onload = function () {
 
